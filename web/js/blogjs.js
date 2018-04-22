9ff4c099c1b2a7161faf20ef776fe1514cfb1b53
MaartenVandenhof-r0613805 <maartenvdh@live.be>
2018-04-22 21:43:45 +0200

    Werkt voor 90%

--- web/js/blogjs.js
+++ web/js/blogjs.js
@@ -5,7 +5,7 @@ var names = document.getElementsByClassName("name");
 var ratings = document.getElementsByClassName("rating");
 var number = 0;
 document.addEventListener("DOMContentLoaded", function(){
-    openSocket();
+
     console.log("Juiste js loaded");
     addComment();
 });
@@ -15,11 +15,11 @@ document.addEventListener("DOMContentLoaded", function(){
 /////////////AddComment/////////////////
 
 function addComment(){
-
+    openSocket();
     for(var i = 0; i<commentbtns.length; i++){
         commentbtns[i].addEventListener('click', function () {
             number = this.id.substr(this.id.length-1);
-            send(commenttext[number-1].value + "@@@@" + names[number-1].value + "####" + ratings[number-1].value);
+            send(commenttext[number-1].value + "@@@@" + names[number-1].value + "####" + ratings[number-1].value + "%%%%" + number);
         })
     }
 }
@@ -32,7 +32,8 @@ var webSocket;
 function openSocket(){
     webSocket = new WebSocket("ws://localhost:8080/comments");
     webSocket.open = function (event) {
-        console.log(names)
+        console.log("the socket is open");
+        writeResponse("Dit is een test")
     };
     webSocket.onmessage = function (event) {
         console.log(event.data);
@@ -58,23 +59,31 @@ function writeResponse(text, i) {
     var rating = document.createElement('h3');
     var seperator1 = "@@@@";
     var seperator2 = "####";
-    var t = text;
+    var seperator3 = "%%%%";
+    var actualNumber;
+    var t;
     commentwrap.setAttribute("class", "commentwrap");
     post.setAttribute("class", "commentpost");
     rating.setAttribute("class", "commentRating");
 
     post.innerHTML = text.split(seperator1).shift();
     t = text.split(seperator1).pop();
+    console.log(t.split(seperator1).pop());
 
     title.innerHTML = t.split(seperator2).shift();
-    rating.innerHTML = t.split(seperator2).pop();
+    t = t.split(seperator2).pop();
 
+    rating.innerHTML = t.split(seperator3).shift();
+    console.log(t.split(seperator3).shift());
+
+    actualNumber = t.split(seperator3).pop();
+    console.log(actualNumber);
 
     commentwrap.appendChild(title);
     commentwrap.appendChild(rating);
     commentwrap.appendChild(post);
 
-    comments[i-1].appendChild(commentwrap);
+    comments[actualNumber-1].appendChild(commentwrap);
 }
 
 
